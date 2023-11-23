2784a74ff0442fc0f8824274ee49d5114f88d23f
chalolowa <wangwelocha@students.uonbi.ac.ke>
2023-11-23 23:24:10 +0300

    On branch main  Initialised express server and routes 	modified:   src/index.ts 	modified:   src/routes.ts 	modified:   src/server.ts

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import route from './routes';
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, 'public')));
+server.use('/css', express.static(path.join(__dirname, 'public/css')));
+server.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
+
+export default server;
+
